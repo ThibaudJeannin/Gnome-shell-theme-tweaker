5b53829d37a59f3742a5a7273cc9c7bf65308486
ThibaudJeannin <thibaud.jeannin@gmail.com>
2017-12-24 20:13:36 +0100

    added copy for Paper icons theme (replace Paper by your icon theme if needed)

--- setIcon.sh
+++ setIcon.sh
@@ -5,12 +5,15 @@ then
   SCRIPT=`realpath $0`
   SCRIPTPATH=`dirname $SCRIPT`
   ICON='ubuntugnome.svg'
+  #ICON='ubuntu.svg'
   FILE=$SCRIPTPATH/$ICON
   if test -f $FILE
   then
     sudo cp $FILE /usr/share/themes/Adapta-Nokto/gnome-shell/assets/show-apps.svg
     sudo cp $FILE /usr/share/themes/Adapta-Nokto/gnome-shell/assets/show-apps-hover.svg
     sudo cp $FILE /usr/share/themes/Adapta-Nokto/gnome-shell/assets/show-apps-checked.svg
+
+    sudo cp $FILE /usr/share/icons/Paper/scalable/actions/view-app-grid-symbolic.svg
     echo Default icon applied
   else
     echo error : missing default file icon : ubuntugnome.svg
@@ -21,11 +24,10 @@ else
     sudo cp $1 /usr/share/themes/Adapta-Nokto/gnome-shell/assets/show-apps.svg
     sudo cp $1 /usr/share/themes/Adapta-Nokto/gnome-shell/assets/show-apps-hover.svg
     sudo cp $1 /usr/share/themes/Adapta-Nokto/gnome-shell/assets/show-apps-checked.svg
+
+    sudo cp $1 /usr/share/icons/Paper/scalable/actions/view-app-grid-symbolic.svg
     echo Icon applied : $1
   else
     echo error : file not found : $1
   fi
 fi
-
-
-#panel .panel-corner { -panel-corner-radius: 8px; -panel-corner-background-color: rgba(19, 25, 28, 1); -panel-corner-border-width: 0; -panel-corner-border-color: transparent; transition-duration: 0s; }
